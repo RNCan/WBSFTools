b41994f6b09ba45a4cbdbf3c224c5da9d2c5db91
Jean-Francois Lavoie <atmailjfl@gmail.com>
2025-10-24 12:10:47 -0400

    Fixed export def for both platforms

--- BioSIM_API/BioSIM_API.h
+++ BioSIM_API/BioSIM_API.h
@@ -4,14 +4,14 @@
 #include <memory>
 #include "Basic/ERMsg.h"
 
-#ifdef _WIN32
-#ifdef BIOSIM_API_EXPORT
-#define DLL_EXPORT __declspec(dllexport)
+#if defined(_WIN32) || defined(_WIN64)
+	#ifdef BIOSIM_API_EXPORT
+	#define DLL_EXPORT __declspec(dllexport)
+	#else
+	#define DLL_EXPORT __declspec(dllimport)
+	#endif
 #else
-#define DLL_EXPORT __declspec(dllimport)
-#endif
-#else
-#define DLL_EXPORT
+	#define DLL_EXPORT __attribute__((visibility("default")))
 #endif
 
 //#define DLL_EXPORT 
